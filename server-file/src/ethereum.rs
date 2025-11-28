869c59d34f88218ee2f55d9acd41c7811d70a776
PearTNhat <letuannhat105@gmail.com>
2025-11-28 01:43:09 +0000

    s

--- server-file/src/ethereum.rs
+++ server-file/src/ethereum.rs
@@ -46,7 +46,7 @@ fn recover_address_from_signature(
     let signature = Signature::try_from(signature_bytes.as_slice())
         .map_err(|e| format!("Invalid signature format: {:?}", e))?;
 
-    let prefix = format!("\x19Ethereum Signed Message:\n{}", message_str.len());
+    let prefix = format!("0x00");
     let full_message = format!("{}{}", prefix, message_str);
     let message_hash = keccak256(full_message.as_bytes());
 
